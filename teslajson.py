900731d05e674def84152ea38c21133e61afa0dc
Seth Robertson <in-github@baka.org>
2018-12-02 15:46:22 -0500

    Clean up teslajson. Move support to library for tokens stored in file retries on error

--- teslajson.py
+++ teslajson.py
@@ -17,81 +17,121 @@ v.command('charge_start')
 try: # Python 3
     from urllib.parse import urlencode
     from urllib.request import Request, build_opener
-    from urllib.request import ProxyHandler, HTTPBasicAuthHandler, HTTPHandler, HTTPSHandler
+    from urllib.request import ProxyHandler, HTTPBasicAuthHandler, HTTPHandler, HTTPSHandler, HTTPError
 except: # Python 2
     from urllib import urlencode
     from urllib2 import Request, build_opener
-    from urllib2 import ProxyHandler, HTTPBasicAuthHandler, HTTPHandler, HTTPSHandler
+    from urllib2 import ProxyHandler, HTTPBasicAuthHandler, HTTPHandler, HTTPSHandler, HTTPError
 import json
-import datetime
-import calendar
+import time
+
+
 
 class Connection(object):
     """Connection to Tesla Motors API"""
     def __init__(self,
-            email='',
-            password='',
-            access_token='',
-            proxy_url = '',
-            proxy_user = '',
-            proxy_password = '',
-            debug = False):
+                 email='',
+                 password='',
+                 access_token='',
+                 tokens_file='',
+                 proxy_url = '',
+                 proxy_user = '',
+                 proxy_password = '',
+                 retries = 0,
+                 retry_delay = 1.5,
+                 debug = False):
         """Initialize connection object
 
         Sets the vehicles field, a list of Vehicle objects
         associated with your account
 
         Required parameters:
-        email: your login for teslamotors.com
-        password: your password for teslamotors.com
+          Option 1: (will log in and get tokens using credentials)
+            email: your login for teslamotors.com
+            password: your password for teslamotors.com
+          Option 2: (will use tokens directly and refresh tokens as needed)
+            tokens_file: File containing json tokens data, will update after refresh
+          Option 3: (use use specified token until it is invalid>
+            access_token
+
+          If you combine option 1&2, it will populate the tokens file
+
 
         Optional parameters:
-        access_token: API access token
         proxy_url: URL for proxy server
         proxy_user: username for proxy server
         proxy_password: password for proxy server
+        retries: Number of times we will retry command on HTTP failure beforing failing
+        retry_delay: Time in seconds we will multiplicatively back off after each failure
+        debug: Turn on debugging of web traffic to tesla (non-proxy case)
         """
+
+        self.tries = retries + 1
+        self.retry_delay = retry_delay
         self.proxy_url = proxy_url
         self.proxy_user = proxy_user
         self.proxy_password = proxy_password
         self.debug = debug
         self.debuglevel = 1 if debug else 0
         self.head = {}
+        self.tokens_file = tokens_file
+        self.access_token = access_token
+        self.refresh_token = None
+
+        # Obtain URL and program access tokens from pastebin
         tesla_client = self.__open("/raw/0a8e0xTJ", baseurl="http://pastebin.com")
+
         self.current_client = tesla_client['v1']
+
+        # Validate that returned URL is going to tesla, to prevent MITM attack
         self.baseurl = self.current_client['baseurl']
         prefix='https://'
         if not self.baseurl.startswith(prefix) or '/' in self.baseurl[len(prefix):] or not self.baseurl.endswith(('.teslamotors.com','.tesla.com')):
             raise IOError("Unexpected URL (%s) from pastebin" % self.baseurl)
+
+        # Prefix for API queries
         self.api = self.current_client['api']
+
         if access_token:
             self._sethead(access_token)
         else:
+            self.expiration = 0 # force refresh
+
             self.oauth = {
                 "grant_type" : "password",
                 "client_id" : self.current_client['id'],
                 "client_secret" : self.current_client['secret'],
                 "email" : email,
                 "password" : password }
-            self.expiration = 0 # force refresh
+
+        if self.tokens_file:
+            with open(self.tokens_file, "r") as R:
+                self._update_tokens(stream=R)
+
         self.vehicles = [Vehicle(v, self) for v in sorted(self.get('vehicles')['response'], key=lambda d: d['id'])]
+
+
 
     def get(self, command):
         """Utility command to get data from API"""
         return self.post(command, None)
 
+
+
     def post(self, command, data={}):
         """Utility command to post data to API"""
-        now = calendar.timegm(datetime.datetime.now().timetuple())
-        if now > self.expiration:
-            auth = self.__open("/oauth/token", data=self.oauth)
-            self._sethead(auth['access_token'],
-                           auth['created_at'] + auth['expires_in'] - 86400)
+        if time.time() > self.expiration:
+            self._refresh_token()
         return self.__open("%s%s" % (self.api, command), headers=self.head, data=data)
 
-    def __user_agent(self):
+
+
+    def _user_agent(self, agent='teslajson.py 1.3.1'):
+        """Set the user agent"""
         if not "User-Agent" in self.head:
-            self.head["User-Agent"] = 'teslajson.py 1.3.1'
+            self.head["User-Agent"] = agent
+
+
 
     def _sethead(self, access_token, expiration=float('inf')):
         """Set HTTP header"""
@@ -99,55 +139,104 @@ class Connection(object):
         self.expiration = expiration
         self.head = {"Authorization": "Bearer %s" % access_token}
 
-    def refresh_token(self, refresh_token):
-        self.oauth = {
-            "grant_type" : "refresh_token",
-            "client_id" : self.current_client['id'],
-            "client_secret" : self.current_client['secret'],
-            "refresh_token" : refresh_token }
+
+
+    def _update_tokens(self, tokens=None, stream=None):
+        """Update tokens from dict or json stream"""
+
+        if stream:
+            tokens = json.load(stream)
+
+        self.access_token = tokens['access_token']
+        self.refresh_token = tokens['refresh_token']
+        self.expiration = tokens["created_at"] + tokens["expires_in"] - 86400
+
+        self._sethead(self.access_token, expiration=self.expiration)
+
+
+
+    def _refresh_token(self):
+        """Refresh tokens using either (preset) email/password or refresh_token"""
+
+        if self.refresh_token:
+            self.oauth = {
+                "grant_type" : "refresh_token",
+                "client_id" : self.current_client['id'],
+                "client_secret" : self.current_client['secret'],
+                "refresh_token" : self.refresh_token }
+
         self.head = {}
-        return self.__open("/oauth/token", data=self.oauth)
+        tokens = self.__open("/oauth/token", data=self.oauth)
+        self._update_tokens(tokens=tokens)
+        if self.tokens_file:
+            with open(self.tokens_file, "w") as W:
+                W.write(json.dumps(tokens))
+
 
 
     def __open(self, url, headers={}, data=None, baseurl=""):
         """Raw urlopen command"""
+
         if not baseurl:
             baseurl = self.baseurl
-        self.__user_agent()
-        req = Request("%s%s" % (baseurl, url), headers=headers)
-        try:
-            req.data = urlencode(data).encode('utf-8') # Python 3
-        except:
-            try:
-                req.add_data(urlencode(data)) # Python 2
-            except:
-                pass
+        self._user_agent()
 
-        # Proxy support
-        if self.proxy_url:
-            if self.proxy_user:
-                proxy = ProxyHandler({'https': 'https://%s:%s@%s' % (self.proxy_user,
-                                                                     self.proxy_password,
-                                                                     self.proxy_url)})
-                auth = HTTPBasicAuthHandler()
-                opener = build_opener(proxy, auth, HTTPHandler)
-            else:
-                handler = ProxyHandler({'https': self.proxy_url})
-                opener = build_opener(handler)
+
+        last_except = Exception
+        for count in range(self.tries):
+            try:
+                req = Request("%s%s" % (baseurl, url), headers=headers)
+                try:
+                    req.data = urlencode(data).encode('utf-8') # Python 3
+                except:
+                    try:
+                        req.add_data(urlencode(data)) # Python 2
+                    except:
+                        pass
+
+                # Proxy support
+                if self.proxy_url:
+                    if self.proxy_user:
+                        proxy = ProxyHandler({'https': 'https://%s:%s@%s' % (self.proxy_user,
+                                                                             self.proxy_password,
+                                                                             self.proxy_url)})
+                        auth = HTTPBasicAuthHandler()
+                        opener = build_opener(proxy, auth, HTTPHandler)
+                    else:
+                        handler = ProxyHandler({'https': self.proxy_url})
+                        opener = build_opener(handler)
+                else:
+                    opener = build_opener(HTTPSHandler(debuglevel=self.debuglevel))
+
+                resp = opener.open(req)
+                charset = resp.info().get('charset', 'utf-8')
+                break
+            except HTTPError as e:
+                last_except = e
+                if self.debug:
+                    print('# %d Timed out or other error for %s: %s\n'%(time.time(),type,str(e)))
+                count += 1
+                if count != self.tries:
+                    time.sleep(count * self.retry_delay)
         else:
-            opener = build_opener(HTTPSHandler(debuglevel=self.debuglevel))
-        resp = opener.open(req)
-        charset = resp.info().get('charset', 'utf-8')
+            raise last_except
+
         return json.loads(resp.read().decode(charset))
+
+
 
 
 class Vehicle(dict):
     """Vehicle class, subclassed from dictionary.
 
-    There are 3 primary methods: wake_up, data_request and command.
-    data_request and command both require a name to specify the data
-    or command, respectively. These names can be found in the
-    Tesla JSON API."""
+    There are 3 primary methods: wake_up, data/data_request and
+    command.  data_request and command both require a name to specify
+    the data or command, respectively.  data gets everything. These
+    names can be found in the Tesla JSON API.
+
+    """
+
+
     def __init__(self, data, connection):
         """Initialize vehicle class
 
@@ -156,10 +245,14 @@ class Vehicle(dict):
         super(Vehicle, self).__init__(data)
         self.connection = connection
 
+
+
     def data_all(self):
         """Get all vehicle data"""
         result = self.get('data')
         return result['response']
+
+
 
     def data_request(self, name):
         """Get vehicle data"""
@@ -169,13 +262,19 @@ class Vehicle(dict):
             result = self.get(name)
         return result['response']
 
+
+
     def wake_up(self):
         """Wake the vehicle"""
         return self.post('wake_up')
 
+
+
     def command(self, name, data={}):
         """Run the command for the vehicle"""
         return self.post('command/%s' % name, data)
+
+
 
     def get(self, command):
         """Utility command to get data from API"""
@@ -183,6 +282,8 @@ class Vehicle(dict):
             return self.connection.get('vehicles/%i/%s' % (self['id'], command))
         else:
             return self.connection.get('vehicles/%i' % (self['id']))
+
+
 
     def post(self, command, data={}):
         """Utility command to post data to API"""
