cbc088e1675fd06d08b52272fd501792f765f402
Pedro Mendes <mendes@copasi.org>
2019-01-12 14:20:38 -0500

    added more elements to tesla_record

--- tesla_parselib.py
+++ tesla_parselib.py
@@ -12,7 +12,13 @@ class tesla_record(object):
         """Create object from json text data from tesla_poller"""
 
         # self.jline set in new
-        self.time = self.jline["retrevial_time"]
+        self.time = 			self.jline["retrevial_time"]
+        self.vehicle_id = 		self.jline["vehicle_id"]
+        self.state = 			self.jline["state"]
+        self.car_locked = 		self._jget(["vehicle_state", "locaked"])
+        self.odometer =			self._jget(["vehicle_state", "odometer"])
+        self.is_user_present =		self._jget(["vehicle_state", "is_user_present"])
+        self.charging_state =		self._jget(["charge_state",  "charging_state"])
         self.usable_battery_level =	self._jget(["charge_state",  "usable_battery_level"])
         self.charge_miles_added =	self._jget(["charge_state",  "charge_miles_added_rated"])
         self.charge_energy_added =	self._jget(["charge_state",  "charge_energy_added"])
@@ -24,8 +30,26 @@ class tesla_record(object):
         self.est_battery_range =	self._jget(["charge_state",  "est_battery_range"])
         self.shift_state =		self._jget(["drive_state",   "shift_state"])
         self.speed =			self._jget(["drive_state",   "speed"])
+        self.latitude =			self._jget(["drive_state",   "latitude"])
+        self.longitude =		self._jget(["drive_state",   "longitude"])
+        self.heading =			self._jget(["drive_state",   "heading"])
+        self.gps_as_of =		self._jget(["drive_state",   "gps_as_of"])
         self.climate_on =		self._jget(["climate_state", "is_climate_on"])
-        self.odometer =			self._jget(["vehicle_state", "odometer"])
+        self.inside_temp =		self._jget(["climate_state", "inside_temp"])
+        self.outside_temp =		self._jget(["climate_state", "outside_temp"])
+        self.battery_heater =		self._jget(["climate_state", "battery_heater"])
+        self.vin =			self.jline["vin"]
+        self.display_name =		self.jline["display_name"]
+        self.car_type =			self._jget(["vehicle_config", "car_type"])
+        self.car_special_type =		self._jget(["vehicle_config", "car_special_type"])
+        self.perf_config =		self._jget(["vehicle_config", "perf_config"])
+        self.has_ludicrous_mode =	self._jget(["vehicle_config", "has_ludicrous_mode"])
+        self.wheel_type =		self._jget(["vehicle_config", "wheel_type"])
+        self.has_air_suspension =	self._jget(["vehicle_config", "has_air_suspension"])
+        self.exterior_color =		self._jget(["vehicle_config", "exterior_color"])
+        self.option_codes =		self._jget(["vehicle_config", "option_codes"])
+        self.car_version =		self._jget(["vehicle_config", "car_version"])
+        
 
         if self.charger_power > 0:
             self.mode = "Charging"
