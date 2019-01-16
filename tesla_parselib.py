14e6d67020506b026569e3d28ccff595017c004c
Pedro Mendes <mendes@copasi.org>
2019-01-16 00:39:05 -0500

    supply dictionaries for insert and update commands

--- tesla_parselib.py
+++ tesla_parselib.py
@@ -108,229 +108,144 @@ class tesla_record(object):
             info = info[key]
         return info
 
-    def sql_vehicle_insert_str(self):
-        result = '({id},\'{vin}\''.format(id=self.vehicle_id, vin=self.vin)
-	if self.display_name:
-	    result = result + ",\'" + self.display_name + "\'"
-	else:
-	    result = result + ",NULL"
-	if self.car_type:
-	    result = result + ",\'" + self.car_type + "\'"
-	else:
-	    result = result + ",NULL"
-	if self.car_special_type:
-	    result = result + ",\'" + self.car_special_type + "\'"
-	else:
-	    result = result + ",NULL"
-        if self.perf_config:
-	    result = result + ",\'" + self.perf_config + "\'"
-	else:
-	    result = result + ",NULL"
-	if self.has_ludicrous_mode is None:
-	    result = result + ",NULL"
-	else:
-	    if self.has_ludicrous_mode:
-	        result = result + ",TRUE"
-	    else:
-	        result = result + ",FALSE"
-        if self.wheel_type:
-	    result = result + ",\'" + self.wheel_type + "\'"
-	else:
-	    result = result + ",NULL"
-	if self.has_air_suspension is None:
-	    result = result + ",NULL"
-        else:
-	    if self.has_air_suspension:
-                result = result + ",TRUE"
-	    else:
-	        result = result + ",FALSE"
-	if self.exterior_color:
-	    result = result + ",\'" + self.exterior_color + "\'"
-        else:
-	    result = result + ",NULL"
-	if self.option_codes:
-	    result = result + ",\'" + self.option_codes + "\'"
-        else:
-	    result = result + ",NULL"
-        if self.car_version:
-	    result = result + ",\'" + self.car_version + "\')"
-	else:
-	    result = result + ",NULL)"
+
+    def sql_vehicle_insert_dict(self):
+	# construct a dictionary with keys and values to insert, to be used in a psycopg2 
+	# vehicle_id and vin need to exist so we just add them
+	result = {}
+        result["vehicle_id"] = self.vehicle_id
+        result["vin"] = self.vin
+	if self.display_name is not None:
+	    result["display_name"] = self.display_name
+	if self.car_type is not None:
+	    result["car_type"] = self.car_type
+	if self.car_special_type is not None:
+	    result["car_special_type"] = self.car_special_type
+        if self.perf_config is not None:
+	    result["perf_config"] = self.perf_config
+	if self.has_ludicrous_mode is not None:
+	    result["has_ludicrous_mode"] = self.has_ludicrous_mode
+        if self.wheel_type is not None:
+	    result["wheel_type"] = self.wheel_type
+	if self.has_air_suspension is not None:
+	    result["has_air_suspension"] = self.has_air_suspension
+	if self.exterior_color is not None:
+	    result["exterior_color"] = self.exterior_color
+	if self.option_codes is not None:
+	    result["option_codes"] = self.option_codes
+        if self.car_version is not None:
+	    result["car_version"] = self.car_version
         return result
 
 
-    def sql_vehicle_update_str(self, current) :
-	# construct a string with the columns that need to be updated ready for 
-	# an SQL update command. We assume vin never changes, so we don't check it
-        result = ""
+    def sql_vehicle_update_dict(self, current) :
+	# construct a dictionary with keys and values to change, to be used in a psycopg2 
+	# update execute command. We assume vin never changes, so we don't check it
+        result = {}
         # check the display_name
         if current[2] != self.display_name:
-	    result = " display_name = '" + self.display_name + "'"
+	    result["display_name"]= self.display_name
 	# check car_type
 	if self.car_type is not None :
 	    if current[3] != self.car_type:
-		if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " car_type = '" + self.car_type + "'"
+		result["car_type"] = self.car_type
 	# check car_special_type
 	if self.car_special_type is not None :
 	    if current[4] != self.car_special_type:
-		if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " car_special_type = '" + self.car_special_type + "'"
+		result["car_special_type"] = self.car_special_type
 	# check perf_config
 	if self.perf_config is not None :
 	    if current[5] != self.perf_config:
-		if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " perf_config = '" + self.perf_config + "'"
+		result["perf_config"] = self.perf_config
 	# check has_ludicrous_mode
 	if self.has_ludicrous_mode is not None :
 	    if current[6] != self.has_ludicrous_mode:
-	        if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " has_ludicrous_mode = " + str(self.has_ludicrous_mode)
+		result["has_ludicrous_mode"] = self.has_ludicrous_mode
 	# check wheel_type
 	if self.wheel_type is not None :
 	    if current[7] != self.wheel_type:
-	        if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " wheel_type = '" + self.wheel_type + "'"
+		result["wheel_type"] = self.wheel_type
 	# check has_air_suspension
 	if self.has_air_suspension is not None :
 	    if current[8] != self.has_air_suspension:
-	        if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " has_air_suspension = " + str(self.has_air_suspension)
+		result["has_air_suspension"] = self.has_air_suspension
 	# check exterior_color
 	if self.exterior_color is not None :
 	    if current[9] != self.exterior_color:
-	        if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " exterior_color = '" + self.exterior_color + "'"
+		result["exterior_color"] = self.exterior_color
 	# check option_codes
 	if self.option_codes is not None :
 	    if current[10] != self.option_codes:
-	        if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " option_codes = '" + self.option_codes + "'"
+		result["option_codes"] = self.option_codes
 	# check car_version
 	if self.car_version is not None :
 	    if current[11] != self.car_version:
-		if( len(result) > 1 ):
-		    result = result + ","
-		result = result + " car_version = '" + self.car_version + "'"
+		result["car_version"] = self.car_version
 	return result
 
 
-    def sql_vehicle_status_insert_str(self):
+    def sql_vehicle_status_insert_dict(self):
+        result = {}
         # make unixtime into date and time
         unix_timestamp = float(self.time)
         local_timezone = tzlocal.get_localzone()
         local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
         ts_str = local_time.strftime("%Y-%m-%d %H:%M:%S")
-        result = '(\'{}\',{},\'{}\''.format(ts_str, self.vehicle_id, self.state)
-        if self.car_locked is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(str(self.car_locked))
-        if self.odometer is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(str(self.odometer))
-        if self.is_user_present is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(str(self.is_user_present))
-        if self.shift_state is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',\'{}\''.format(self.shift_state)
-        if self.speed is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.speed)
-        if self.latitude is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.latitude)
-        if self.longitude is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.longitude)
-        if self.heading is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.heading)
-        if self.gps_as_of is None :
-	    result = result + ",NULL"
-	else :
+        result["ts"] = ts_str
+        result["vehicle_id"] = self.vehicle_id
+        result["state"] = self.state
+        if self.car_locked is not None :
+	    result["car_locked"] = self.car_locked
+        if self.odometer is not None :
+	    result["odometer"] = self.odometer
+        if self.is_user_present is not None :
+	    result["is_user_present"] = self.is_user_present
+        if self.shift_state is not None :
+	    result["shift_state"] = self.shift_state
+        if self.speed is not None :
+	    result["speed"] = self.speed
+        if self.latitude is not None :
+	    result["latitude"] = self.latitude
+        if self.longitude is not None :
+	    result["longitude"] = self.longitude
+        if self.heading is not None :
+	    result["heading"] = self.heading
+        if self.gps_as_of is not None :
             # make gps_as_of unixtime into date and time
             unix_timestamp = float(self.gps_as_of)
             local_timezone = tzlocal.get_localzone()
             local_time = datetime.fromtimestamp(unix_timestamp, local_timezone)
             gps_str = local_time.strftime("%Y-%m-%d %H:%M:%S")
-            result = result + ',\'{}\''.format(gps_str)
-        if self.charging_state is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',\'{}\''.format(self.charging_state)
-        if self.usable_battery_level is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.usable_battery_level)
-        if self.battery_range is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.battery_range)
-        if self.est_battery_range is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.est_battery_range)
-        if self.charge_rate is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.charge_rate)
-        if self.charge_miles_added is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.charge_miles_added)
-        if self.charge_energy_added is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.charge_energy_added)
-        if self.charge_current_request is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.charge_current_request)
-        if self.charger_power is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.charger_power)
-        if self.charger_voltage is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.charger_voltage)
-        if self.inside_temp is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.inside_temp)
-        if self.outside_temp is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(self.outside_temp)
-        if self.climate_on is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(str(self.climate_on))
-        if self.battery_heater is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(str(self.battery_heater))
-        if self.valet_mode is None :
-	    result = result + ",NULL"
-	else :
-	    result = result + ',{}'.format(str(self.valet_mode))
-        result = result + ')'
+            result["gps_as_of"] = gps_str
+        if self.charging_state is not None :
+	    result["charging_state"] = self.charging_state
+        if self.usable_battery_level is not None :
+	    result["battery_level"] = self.usable_battery_level
+        if self.battery_range is not None :
+	    result["battery_range"] = self.battery_range
+        if self.est_battery_range is not None :
+	    result["est_battery_range"] = self.est_battery_range
+        if self.charge_rate is not None :
+	    result["charge_rate"] = self.charge_rate
+        if self.charge_miles_added is not None :
+	    result["miles_added"] = self.charge_miles_added
+        if self.charge_energy_added is not None :
+	    result["energy_added"] = self.charge_energy_added
+        if self.charge_current_request is not None :
+	    result["charge_current_request"] = self.charge_current_request
+        if self.charger_power is not None :
+	    result["charger_power"] = self.charger_power
+        if self.charger_voltage is not None :
+	    result["charger_voltage"] = self.charger_voltage
+        if self.inside_temp is not None :
+	    result["inside_temp"] = self.inside_temp
+        if self.outside_temp is not None :
+	    result["outside_temp"] = self.outside_temp
+        if self.climate_on is not None :
+	    result["climate_on"] = self.climate_on
+        if self.battery_heater is not None :
+	    result["battery_heater"] = self.battery_heater
+        if self.valet_mode is not None :
+	    result["valet_mode"] = self.valet_mode
         return result
       
