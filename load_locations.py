cc03af0973d5e330c8112a2dacce45dc62dd7a33
Pedro Mendes <mendes@copasi.org>
2019-04-28 21:15:12 -0400

    checkguard for verbose argument

--- load_locations.py
+++ load_locations.py
@@ -23,6 +23,9 @@ parser.add_argument('--dbconfig', type=str, default='dbconfig', help='Insert rec
 parser.add_argument('tsvfile', type=str, help="file with data in tsv format")
 args = parser.parse_args()
 
+if args.verbose is None:
+    args.verbose = 0
+
 # connect to the database
 if args.dbconfig:
     # we are going to read data from the database
@@ -38,12 +41,15 @@ if args.dbconfig:
         cursor.execute("SELECT version();")
         record = cursor.fetchone()
         if args.verbose:
-            print('Connected to {}\n'.format(str(record[0])))
+            print('Connected to {}'.format(str(record[0])))
         #cursor.close()
     except (Exception, psycopg2.Error) as error :
         print("Error while connecting to PostgreSQL", error)
         exit()
         
+    # initialize counters
+    totlocs = 0
+    addedlocs = 0
     # read TSV file
     with open(args.tsvfile) as infile:
         reader = csv.reader(infile, delimiter='\t')
@@ -54,12 +60,14 @@ if args.dbconfig:
         query = sql.SQL("SELECT location_id FROM location WHERE name={} AND latitude={} AND longitude={} AND is_tesla_supercharger={} AND is_charge_station = {} AND is_home = {} AND is_work = {}").format(sql.Placeholder(), sql.Placeholder(), sql.Placeholder(), sql.Placeholder(), sql.Placeholder(), sql.Placeholder(), sql.Placeholder() )
         # process each entry
         for row in reader:
+	    totlocs = totlocs + 1
 	    # check if this is already in the database
 	    if args.verbose>2:
 	        print cursor.mogrify(query,[row[0], row[1], row[2], row[3], row[4], row[5], row[6] ] )
 	    cursor.execute(query,[row[0], row[1], row[2], row[3], row[4], row[5], row[6] ] )
 	    # if there is no match we insert it
 	    if cursor.rowcount<1:
+	        addedlocs = addedlocs + 1
 	        if args.verbose>2:
 	            print cursor.mogrify(insert_str,{"name": row[0], "latitude": row[1], "longitude": row[2], "is_tesla_supercharger": row[3], "is_charge_station": row[4], "is_home": row[5], "is_work": row[6]})
                 try:
@@ -71,12 +79,22 @@ if args.dbconfig:
                     dbconn.commit()
             else:
 	        if args.verbose>0:
-	            print('skipping duplicate ({}, {}, {})\n'.format(row[0], str(row[1]), str(row[2])))
-
+		    outstr = 'skipping duplicate ({}, {}, {}'.format(row[0], str(row[1]), str(row[2]))
+	            if row[3] == 't': 
+		        outstr = outstr + ', Tesla supercharger'
+		    if row[4] == 't':
+		        outstr = outstr + ', Charging station'
+		    if row[5] == 't':
+		        outstr = outstr + ', Home'
+		    if row[6] == 't':
+		        outstr = outstr + ', Work'
+		    outstr = outstr + ')'
+	            print(outstr)
 
     if(dbconn):
         cursor.close()
         dbconn.close()
         if args.verbose>0:
-            print "PostgreSQL connection closed\n"
+            print "PostgreSQL connection closed"
+    print "Added " + str(addedlocs) + " new locations (from " + str(totlocs) + " total)"
 
